3d44d6e8425b7ded3d2545d7d1126b56346e50ef
Wanda <wmmcinturff@gmail.com>
2020-07-27 19:00:55 -0500

    setup and practice event listeners, etc.

---
+++ main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+const whatIsYourSign = (sign) => {
+    return `My sign is ${"sign"}.`;
+}
+
+const yourBirthMonth = (month) => { 
+    return `My birth month is ${"month"}.`;
+}
+
+const mySignIs = (sign, yourBirthMonth) => {
+    return 
+}
+
+//let myBtn = document.getElementById('main-btn');
+
+//console.log('myBtn')
+
+
+const mainBtn = document.querySelector('#main-btn');
+
+div.addEventListener ("click", () => {
+    console.log(clickMeSummons);
+})
+
+const clickMeSummons = () => {
+    console.log('You clicked me!');
+}
